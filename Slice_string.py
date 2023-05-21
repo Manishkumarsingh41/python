b0d3503f6a1fb4373393b1117fdf4ce404d2c5ef
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-05-21 15:03:33 +0530

    Add files via upload
    Use of slice string

---
+++ Slice_string.py
@@ -0,0 +1,10 @@
+#Slicing Example
+name="Manish"
+namelen=len(name)
+print(name[0:6])
+print(name[1:6])
+print(name[:6])
+print(name[0:-3])
+print(name[0:len(name)-3])
+print(name[-6:len(name)-3])
+print(name[-3:-1])
