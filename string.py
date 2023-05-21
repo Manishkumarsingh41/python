dff3ccbd6089d74830da232103d57263bf30922e
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-05-21 14:20:38 +0530

    Add files via upload
    Use of strings in python

---
+++ string.py
@@ -0,0 +1,4 @@
+apple = '''In python, anything that you enclose between single or double quotation marks is considered a string. A string is essentially a sequence or array of textual data. Strings are used when working with Unicode characters'''
+print("Lets use a for loop\n")
+for character in apple:
+    print(character)
