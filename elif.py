6f77dd7d486f0fe243b7c29c5bd0d3c78a1c2d25
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-05-23 15:52:47 +0530

    Add files via upload
    Use of elif Statements in python

---
+++ elif.py
@@ -0,0 +1,10 @@
+num = int(input("Enter your number: "))
+#print("Your age is:", a)
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+else:
+    print("Number is positive.")
+    
+print("I am happy")
