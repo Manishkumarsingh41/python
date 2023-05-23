85bc0677dfcf651f0640c5f190feab632a766491
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-05-23 16:47:28 +0530

    Add files via upload
    A python program ,which is  capable to show  Date ,Time and Day .

---
+++ watch.py
@@ -0,0 +1,10 @@
+import time
+timestamp = time.strftime('%D')
+print("Date      :-",timestamp)
+timestamp = time.strftime('%I:%M:%S:%p')
+print("Times now :-",timestamp)
+timestamp = time.strftime('%A')
+print("Today is  :-",timestamp) 
+
+
+print("       THANK YOU")
