6a1b3294eb435f92b2ddfb6595e34a1e858013a8
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-05-24 16:36:00 +0530

    Add files via upload
    Use of  while Loop and Else with While Loop in python.

---
+++ while loop and else with while loop.py
@@ -0,0 +1,13 @@
+print('while Loop ')
+count = 5
+while (count > 0):
+  print(count)
+  count = count - 1
+
+print('Else with While Loop ')
+x = 5
+while (x > 0):
+    print(x)
+    x = x - 1
+else:
+    print('counter is 0')
