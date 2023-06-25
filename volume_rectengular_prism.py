35b27fd1879f20d7f40036f7b05ec094121976a6
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-06-25 11:18:05 +0530

    Add files via upload
     this programming challenge, you will be creating a function that calculates the volume of a rectangular prism in cubic feet.  The formula to find the volume of a rectangular prism is V = l * w * h where l, w, and h are length, width, and height, respectively.
    
    First, create three variables representing length, width, and height.   Assign each of them an integer as user input using the input() function and int().
    
    Next, create a function to calculate the volume of the rectangular prism.  It should have 3 parameters representing length, width, and height and return the volume of a rectangular prism calculated using those 3 parameters.
    
    Finally, use print() to display "The volume of the rectangular prism is [call function  here to calculate volume] cubic feet." in the output.  You will have to use str() on the function call to be able to concatenate it with strings since it returns an integer.

---
+++ volume_rectengular_prism.py
@@ -0,0 +1,5 @@
+length = int(input("Enter the length of the rectangular prism: "))
+width = int(input("Enter the width of the rectangular prism: "))
+height = int(input("Enter the height of the rectangular prism: "))
+volume=length*width*height
+print("The volume of the rectangular prism is " + str(volume) + " cubic feet.")
