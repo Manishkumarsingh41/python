73fe2ca7620306f142ddaee0b18959451a89799e
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-07-14 00:30:41 +0530

    Add files via upload
    In this python program take two  user input in between  which we find a number which is divisible by 1 to 10 all number .
    for example :- we take first number is 1 and second number is 3000 the output is 2520 this number is divisible by all number from 1 to 10.

---
+++ number divided by one to ten.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number\n"))
+b=int(input("Enter second number\n"))
+number=0
+for num in range(a,b):
+    if num%2==0 and num%3==0 and num%4==0 and num%5==0 and num%6==0 and num%7==0 and num%8==0 and num%9==0 and num%10==0:
+        print (num)
+        number+=1
+else:
+    print("Enter second number bigger then")
