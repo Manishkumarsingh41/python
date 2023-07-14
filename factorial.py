dc7eaca70180867da78ce35402de465b7674bd3d
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-07-15 00:31:54 +0530

    Add files via upload
    this python program print factorial of a number.

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(fac_num):
+    returned=1
+    for item in range(fac_num,1,-1):
+        returned*=item
+      
+    return returned
+print(factorial(3))
+print(factorial(4)) 
+print(factorial(5))
+    
+    
