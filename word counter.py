cd761b1c21df05c7ef007ba38781fd640203d9dd
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-07-20 00:22:54 +0530

    Add files via upload
    Word Counter program using python,in this program we input the string and then  run the program in output the value of  number of words in string is show.

---
+++ word counter.py
@@ -0,0 +1,13 @@
+str_1 =str(input("Enter any sentance and words:\n"))
+def word_counter(words):
+    spaces_and_letters = ""
+    word_count = 1
+    for i in words:
+        if i.isalnum() or i.isspace() or i == "-" or i == "'":
+            spaces_and_letters += i
+    for j in spaces_and_letters:
+        if j == " ":
+            word_count += 1
+    return word_count
+ 
+print(word_counter(str_1))
