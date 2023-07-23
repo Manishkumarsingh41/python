22812d2f6df0ba0fcc1f7f7195d58b2965784ca2
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-07-23 15:29:02 +0530

    Add files via upload
    A python program take user input and  calculate bonous of employe according to their experiance in companey .

---
+++ bonous_calculator.py
@@ -0,0 +1,11 @@
+name=input("Enter employe name:-")
+year_of_join=int(input("Enter year when you join the company"))
+exp=2023-year_of_join
+if exp>=5:
+    print("You are elegible for bonous")
+    sal=int(input("Enter your salery:-"))
+    bonous_percentage=float(input("Enter bonous percentage:-"))    
+    bonous=sal*(bonous_percentage/100)+sal
+    print("The salary of ",name,"after adding bonous is ",bonous,)
+else:
+    print("You are not elegible for bonous ")
