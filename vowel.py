9b81221a07bd546405959ff757417435e6f1ba44
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-07-25 22:10:18 +0530

    Add files via upload
    python program to check  vowel and consonant.

---
+++ vowel.py
@@ -0,0 +1,8 @@
+cha =input("Enter a character:-")
+if(cha =='a' or cha=='A'or cha=='e'or cha=='E'or cha=='i'or cha=='I'or cha=='o'or cha=='O'or cha=='u'or cha=='U'):
+      
+    print("it is  vowel")
+      
+else:
+    
+    print("it is consonant")
