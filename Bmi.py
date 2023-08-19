4c89495a39c64836221708c48a0a67365e813183
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-08-19 23:02:31 +0530

    Add files via upload
    The Body Mass Index (BMI) is a measure of body fat based on height and weight. It is commonly used to classify underweight, overweight, and obesity in adults. The formula for calculating BMI is:
    
    BMI = weight (kg) / height (m)^2
    
    This Python code calculates BMI of a body using the above formula.

---
+++ Bmi.py
@@ -0,0 +1,12 @@
+def add(a, b):
+  """Adds two numbers together.
+
+  Args:
+    a: The first number to add.
+    b: The second number to add.
+
+  Returns:
+    The sum of a and b.
+  """
+
+  return a + b
