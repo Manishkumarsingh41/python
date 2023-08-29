adc0daf5fc9e853589d142360479d79efe96e62a
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-08-29 20:10:32 +0530

    Add files via upload
    This Python code , dictionaries are exemplified by generating, modifying, and iterating through them. The `fromkeys()` method is utilized to assign keys, while the `pop()` and `popitem()` methods are employed to eliminate key-value pairs. Additionally, the `print()` function is used to display information on the console. The script presents multiple techniques for managing dictionaries in Python.

---
+++ dictionary methods 2.py
@@ -0,0 +1,8 @@
+for key, value in {}.fromkeys("bcdfghjklmnpqrstvwxyz", "consonant").items():
+    print(key, value)
+ 
+fast_food_items = {"McDonald's": "Big Mac", "Burger King": "Whopper", "Chick-fil-A": "Original Chicken Sandwich"}
+print(fast_food_items.pop("McDonald's"))
+ 
+fast_food_items.popitem()
+print(fast_food_items)
