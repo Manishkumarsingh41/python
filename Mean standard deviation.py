6df0ffd2b545c2adbc021f6541bc618a85af65be
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-09-04 22:09:05 +0530

    Add files via upload
    This code shows how to calculate the mean, variance and standard deviation of a list of numbers in Python,The code imports the necessary libraries, creates a list of numbers, calculates the mean, variance and standard deviation using the appropriate functions, and prints the results.

---
+++ Mean standard deviation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data = [1, 2, 3, 4, 5]
+
+mean = np.mean(data)
+variance = np.var(data)
+std_dev = np.std(data)
+
+print("Mean:", mean)
+print("Variance:", variance)
+print("Standard Deviation:", std_dev)
