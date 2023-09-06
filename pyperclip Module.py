831a252026906b7d3114ef963dfc14f522c2064e
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-09-06 12:19:14 +0530

    Add files via upload
    The pyperclip module in Python provides a convenient way to copy and paste strings. It has two functions: copy() and paste().
    
    The copy() function copies the specified string to the clipboard. The paste() function pastes the contents of the clipboard to the current position of the cursor.

---
+++ pyperclip Module.py
@@ -0,0 +1,26 @@
+import pyperclip
+
+# Copy a string to the clipboard
+pyperclip.copy("This is a string.")
+
+# Paste the string
+print(pyperclip.paste())
+
+
+def read_file(filename):
+  """Reads the contents of a text file and returns it as a string."""
+  with open(filename, "r") as f:
+    return f.read()
+
+
+def main():
+  """Reads the contents of a text file and prints it to the console."""
+  filename = input("Enter the filename: ")
+  contents = read_file(filename)
+  print(contents)
+
+
+if __name__ == "__main__":
+  main()
+
+
