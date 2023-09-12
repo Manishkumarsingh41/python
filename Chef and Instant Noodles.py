0e76aa745ec5bfe89dff214e47623f90e739fcd7
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-09-12 10:43:48 +0530

    Add files via upload
    A Chef has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook.Chef's restaurant has X stoves and only 1 packet can be cooked in a single stove at any minute.How many customers can Chef serve in Y minutes if each customer orders exactly 1 packet of noodles?

---
+++ Chef and Instant Noodles.py
@@ -0,0 +1,4 @@
+'''A Chef has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook.Chef's restaurant has X stoves and only 1 packet can be cooked in a single stove at any minute.How many customers can Chef serve in Y minutes if each customer orders exactly 1 packet of noodles?'''
+
+x,y=map(int,input().split())
+print(x*y)
