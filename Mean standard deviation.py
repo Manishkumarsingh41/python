a84f94b224a128919bf8f828592ff0dc836285c2
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-09-28 22:07:58 +0530

    Mean standard deviation.py

--- Mean standard deviation.py
+++ Mean standard deviation.py
@@ -4,8 +4,7 @@ data = [1, 2, 3, 4, 5]
 
 mean = np.mean(data)
 variance = np.var(data)
-std_dev = np.std(data)
-
+std_dev = np.std(data)
 print("Mean:", mean)
 print("Variance:", variance)
 print("Standard Deviation:", std_dev)
