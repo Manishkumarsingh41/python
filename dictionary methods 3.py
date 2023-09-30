be5272f1f5d174d00f40bc3ac6fe1adfa679768c
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-09-30 22:35:45 +0530

    dictionary methods 3.py

--- dictionary methods 3.py
+++ dictionary methods 3.py
@@ -4,4 +4,4 @@ internet_celebrities.update(another_one)  # 2
 gamers = internet_celebrities.copy()  # 3
 internet_celebrities.clear()  # 4
 print(internet_celebrities)  # 5
-print(gamers)  # 6
+print(gamers) # 6
