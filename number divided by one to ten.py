8a48d94e3986e80f664be31f41fb3134be6248b1
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-10-12 23:01:36 +0530

    number divided by one to ten.py

--- number divided by one to ten.py
+++ number divided by one to ten.py
@@ -1,5 +1,5 @@
-a=int(input("Enter first number\n"))
-b=int(input("Enter second number\n"))
+a=int(input("Enter first number:-\n"))
+b=int(input("Enter second number:-\n"))
 number=0
 for num in range(a,b):
     if num%2==0 and num%3==0 and num%4==0 and num%5==0 and num%6==0 and num%7==0 and num%8==0 and num%9==0 and num%10==0:
